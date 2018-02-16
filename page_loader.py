b7ee616f4c1971daaf3ba9c3bf23210a0249bde4
alihanks <alihanks@gmail.com>
2018-02-16 12:01:41 -0800

    changes to show new data at stationary display stations

--- page_loader.py
+++ page_loader.py
@@ -14,11 +14,13 @@ if __name__ == "__main__":
     		'https://radwatch.berkeley.edu/dosenet/map#dosenet_rad_banner',
     		'https://radwatch.berkeley.edu/dosenet/data#chartdata',
     		'https://radwatch.berkeley.edu/dosenet/data#alldata',
+            'https://radwatch.berkeley.edu/dosenet/schools/etcheverry#d3s_data',
+            'https://radwatch.berkeley.edu/dosenet/schools/etcheverry#weather_data',
     		'file:///home/pi/dosenet-web/display-monitors/WeatherStation.html',
             'file:///home/pi/dosenet-web/display-monitors/FindMore.html']
     sleeps = [10,30,10,10,30,30,30,20,15]
 
-sleep(30)
+sleep(45)
 profile = webdriver.FirefoxProfile()
 extensions = [join(firefox_extension_path, f) for f in listdir(firefox_extension_path) if isfile(join(firefox_extension_path, f))]
 for f in extensions:
